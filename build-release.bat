6a78197830fbb266e29c3c5d0b96abe24fdb4ed5
Matěj Zábský <mzabsky@gmail.com>
2023-04-07 15:10:54 +0200

    ZMP and C preprocessor are no longer necessary.

--- build-release.bat
+++ build-release.bat
@@ -4,17 +4,10 @@ if not exist "temp" mkdir temp
 if not exist "temp\acs" mkdir temp\acs
 if not exist "temp\decorate" mkdir temp\decorate
 if not exist "output" mkdir output
-echo ==============================================================
-utility\mcpp "acs\_main.acs" -o temp\acs\processed.acs -D ZDOOM -D IgnoreHash(x)=x -P
-echo ==============================================================
-utility\zmp -d decorate -a "temp\acs\processed.acs" -o temp\acs\zmp_processed.acs -p temp/decorate/decorate.txt -m zdoom -l HELLMUTATION_ACS
-if not errorlevel 0 (
-	pause
-	exit
-)
+
 echo ==============================================================
 
-utility\acc temp\acs\zmp_processed.acs temp\acs\HELLMUTATION_ACS
+utility\acc acs\_main.acs temp\acs\HELLMUTATION_ACS
 if exist "temp\acs\acs.err" (
 	echo ==============================================================
 	move /y temp\acs\acs.err acs_errors_zdoom.log	
